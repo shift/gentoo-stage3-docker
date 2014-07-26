b90bbba657413d682b3f46c86c341597c2a93d17
Vincent Palmer <shift@someone.section.me>
2014-07-26 13:30:55 +0000

    Fixed auto-tagging.

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@ LATEST_VERSION=$LATEST_VERSION
 
 if [ -z "$LATEST_VERSION" ]; then
   LATEST_VERSION=$LATEST_VERSION`curl 'http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64.txt' | tail -n 1`
+  TAG=autobuild-`echo $LATEST_VERSION | sed -n 's|\(.*\)\/.*|\1|p'`
 fi
 
 #wget -c "http://distfiles.gentoo.org/releases/amd64/autobuilds/${LATEST_VERSION}"
 mkdir rootfs
-git tag ${LATEST_VERSION}
+git tag $TAG
 sed -ri 's/LATEST_VERSION=$LATEST_VERSION/LATEST_VERSION=$LATEST_VERSION/' build.sh
 git add build.sh
 git push origin --tags
