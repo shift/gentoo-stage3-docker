c86fb0766211802822f632ddcbccad63c59d79cf
Vincent Palmer <shift@someone.section.me>
2014-07-26 13:40:27 +0000

    Removed auto-tagging.

--- build.sh
+++ build.sh
@@ -2,16 +2,17 @@
 LATEST_VERSION=
 
 if [ -z "$LATEST_VERSION" ]; then
-  LATEST_VERSION=$LATEST_VERSION`curl 'http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64.txt' | tail -n 1`
+  LATEST_VERSION=autobuild-20140724$LATEST_VERSION`curl 'http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64.txt' | tail -n 1`
   TAG=autobuild-`echo $LATEST_VERSION | sed -n 's|\(.*\)\/.*|\1|p'`
 fi
 
-#wget -c "http://distfiles.gentoo.org/releases/amd64/autobuilds/${LATEST_VERSION}"
+wget -c "http://distfiles.gentoo.org/releases/amd64/autobuilds/${LATEST_VERSION}"
 mkdir rootfs
-git tag $TAG
-sed -ri "s/LATEST_VERSION=$LATEST_VERSION/LATEST_VERSION=$LATEST_VERSION/" build.sh
-git add build.sh
-git push origin --tags
+#git tag $TAG
+#sed -ri "s/LATEST_VERSION=autobuild-20140724/LATEST_VERSION=$TAG/" build.sh
+#git add build.sh
+#git push origin --tags
 (cd rootfs && sudo tar xvjpf ../*.tar.bz2)
+sudo docker rmi shift/gentoo-stage3-amd64
 sudo tar -C rootfs -c . | sudo docker import - shift/gentoo-stage3-amd64
 sudo docker push shift/gentoo-stage3-amd64
